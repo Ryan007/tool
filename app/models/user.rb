f72d50b3b464ae3341fe9847439371e947f87dd2
ryan007 <goodboyryan007@gmail.com>
2013-01-02 09:29:01 +0800

    测试自动发送邮件

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,4 +17,29 @@ class User < ActiveRecord::Base
       :admin, :sign_in_count, :password_salt, :confirmed_at, :confirmation_sent_at, :created_at, :updated_at,
       :encrypted_password, :last_sign_in_at
   end
+
+  def self.test
+    User.new(:email => "helloworld", :username => "zzzhhh")
+    puts "helloworld-------------------#{Time.now}"
+  end
+
+  def self.send_bbs_core_mail(email1)
+      # 验证email
+    before_at = /([a-zA-Z0-9]+(_?[a-zA-Z0-9])+)/  
+    after_at = /(\w)+((\.\w{2,3}){1,3})+/  
+    email = /^(#{before_at}@#{after_at})$/ 
+    # email1 = params[:email]
+    # 校验email字段
+    if email1
+      if (email =~ email1).nil?
+          flash[:notice] = '发送失败，您的email地址不合法!'
+        else
+          SendBbs.core_customer_mailer(email1).deliver
+          flash[:notice] = '发送成功!'
+      end
+      redirect_to root_url
+    end
+  end
+
+
 end
