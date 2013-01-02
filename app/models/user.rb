45f9b47c7181443b38912254144fa03402f7ff3b
ryan007 <goodboyryan007@gmail.com>
2013-01-02 09:37:33 +0800

    添加encoding

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
@@ -18,13 +19,8 @@ class User < ActiveRecord::Base
       :encrypted_password, :last_sign_in_at
   end
 
-  def self.test
-    User.new(:email => "helloworld", :username => "zzzhhh")
-    puts "helloworld-------------------#{Time.now}"
-  end
-
   def self.send_bbs_core_mail(email1)
-      # 验证email
+    # 验证email
     before_at = /([a-zA-Z0-9]+(_?[a-zA-Z0-9])+)/  
     after_at = /(\w)+((\.\w{2,3}){1,3})+/  
     email = /^(#{before_at}@#{after_at})$/ 
@@ -40,6 +36,5 @@ class User < ActiveRecord::Base
       redirect_to root_url
     end
   end
-
 
 end
