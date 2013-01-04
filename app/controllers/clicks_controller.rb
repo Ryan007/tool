7c5e9cc390a2775b2c1260ab6d898f5891e52d6d
ryan007 <goodboyryan007@gmail.com>
2013-01-04 09:35:14 +0800

    增加核心用户查询功能

--- app/controllers/clicks_controller.rb
+++ app/controllers/clicks_controller.rb
@@ -329,9 +329,16 @@ class ClicksController < ApplicationController
 
   # bbs核心用户
   def bbs_core_customer
-    time_range = (Time.now.midnight - 1.day)..Time.now.midnight
+    if !params[:start_date].empty?
+      @t = DateTime.strptime(params[:start_date] + " CCT", "%Y-%m-%d")
+      @time = @t.strftime("%Y-%m-%d")
+      time_range = ((@t.midnight + 1.second) - 1.day)..@t.midnight
+    else
+      time_range = (Time.now.midnight - 1.day)..Time.now.midnight
+    end
+
     @customers = BbsCoreCustomer.where('current_date' => time_range).paginate(:page => params[:page], :per_page => 20)
-    # @click_count = Click.where('current_date' => time_range).sum(:clicks)
+    @total = BbsCoreCustomer.where('current_date' => time_range)
     respond_to do |format|
       format.html # index.html.erb
     end
