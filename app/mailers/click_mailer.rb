fc497238021e811076bd67f60e15e0c255fcacbc
ryan007 <goodboyryan007@gmail.com>
2013-01-29 11:44:16 +0800

    修改rollback

---
+++ app/mailers/click_mailer.rb
@@ -0,0 +1,92 @@
+# encoding: utf-8
+class ClickMailer < ActionMailer::Base
+  default :from => "noreply@xiaoma.com"
+
+  @@arr = []
+  def send_traffic(recipient)
+    # # 先生成excel文件
+    time_range = (Time.now.midnight - 1.day)..Time.now.midnight
+    puts time_range
+    @yd = Time.now - 1.day
+    @@arr = []
+    traverse_dir("#{Rails.root}/download/triffs/")
+    # 计算总的点击次数 uv
+    @count_clicks = Click.where('record_date' => time_range).sum("clicks")
+
+    # if !@@arr.include?"#{@yd.strftime('%Y-%m-%d')}-ReferralTraffic.xls"
+    #     @triffs = ReferralTraffic.where('current_date' => time_range).order("clicks DESC")
+    #     export_xls(@triffs, ReferralTraffic, @count_uv, @yd)
+    # end
+    # if !@@arr.include?"#{@yd.strftime('%Y-%m-%d')}-OrganicTraffic.xls"
+    #     @triff1s = OrganicTraffic.where('current_date' => time_range).order("clicks DESC")
+    #     export_xls(@triff1s, OrganicTraffic, @count_uv1, @yd)
+    # end
+    if !@@arr.include?"#{@yd.strftime('%Y-%m-%d')}-Campaign.xls"
+        all = Click.select("*, sum(clicks) as sum_campaign").group("campaign").where('record_date' => time_range).order("sum_campaign DESC")
+        export_click_xls(all, @count_clicks, Click, @yd)
+    end
+    
+    @recipients = recipient
+    @from = 'noreply@xiaoma.com'
+    @subject = "流量数据统计表"
+    
+    # attachments["引荐流量#{@yd.strftime('%Y-%m-%d')}.xls"] = File.read("#{Rails.root}/download/triffs/#{@yd.strftime('%Y-%m-%d')}-ReferralTraffic.xls")
+    # attachments["自然流量#{@yd.strftime('%Y-%m-%d')}.xls"] = File.read("#{Rails.root}/download/triffs/#{@yd.strftime('%Y-%m-%d')}-OrganicTraffic.xls")
+    attachments["广告位统计#{@yd.strftime('%Y-%m-%d')}.xls"] = File.read("#{Rails.root}/download/triffs/#{@yd.strftime('%Y-%m-%d')}-Click.xls")
+
+    mail(:to => "#{recipient}", :subject => @subject)
+  end
+
+  # 查询目录下面的文件
+  def traverse_dir(file_path)
+      if File.directory? file_path
+          Dir.foreach(file_path) do |file|
+              if file !="." and file !=".."
+                  traverse_dir(file_path+"/"+file)
+              end
+          end
+      else
+          @@arr << "#{File.basename(file_path)}"
+      end
+  end
+
+  private  
+    # 导出流量数据
+    def export_click_xls(objs, count_all, model, yd)
+      count_row = 0
+      count_col = 0
+      xls_report = StringIO.new  
+      book = Spreadsheet::Workbook.new
+
+      sheet1 = book.create_worksheet :name => "根据页面显示" 
+      sheet2 = book.create_worksheet :name => "根据活动显示"
+        
+      blue = Spreadsheet::Format.new :color => :blue, :weight => :bold, :size => 10  
+      sheet1.row(0).default_format = blue  
+      
+      sheet1[0, 0] = '名称'
+      sheet1[0, 1] = '点击量'
+
+      sheet2[0, 0] = '名称'
+      sheet2[0, 1] = '点击量'
+
+      sheet1[1, 1] = count_all
+      count_row = count_row + 2
+      
+      objs.each do |obj|
+        [0, 1].each do |col|
+          if col == 0
+            sheet1[count_row,col]=Campaign.find(obj.campaign).name
+          else
+            sheet1[count_row,col]=obj.sum_campaign
+          end
+        end
+        count_row = count_row + 1
+      end  
+      
+      filepath=Rails.root+"download/triffs/#{(yd).strftime('%Y-%m-%d')}-#{model}.xls" 
+      book.write filepath
+      xls_report.string
+    end
+
+end
