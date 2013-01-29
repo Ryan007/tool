3994e9096a0d3d0ff3d364b1837e04eaaed49459
ryan007 <goodboyryan007@gmail.com>
2013-01-29 14:56:28 +0800

    每日点击量自动发送

--- app/mailers/click_mailer.rb
+++ app/mailers/click_mailer.rb
@@ -22,8 +22,7 @@ class ClickMailer < ActionMailer::Base
     #     export_xls(@triff1s, OrganicTraffic, @count_uv1, @yd)
     # end
     if !@@arr.include?"#{@yd.strftime('%Y-%m-%d')}-Campaign.xls"
-        all = Click.select("*, sum(clicks) as sum_campaign").group("campaign").where('record_date' => time_range).order("sum_campaign DESC")
-        export_click_xls(all, @count_clicks, Click, @yd)
+        export_click_xls(@count_clicks, Click, @yd)
     end
     
     @recipients = recipient
@@ -51,42 +50,76 @@ class ClickMailer < ActionMailer::Base
   end
 
   private  
-    # 导出流量数据
-    def export_click_xls(objs, count_all, model, yd)
-      count_row = 0
-      count_col = 0
-      xls_report = StringIO.new  
-      book = Spreadsheet::Workbook.new
+	    # 导出流量数据
+	    def export_click_xls(count_all, model, yd)
+	    	time_range = (Time.now.midnight - 1.day)..Time.now.midnight
 
-      sheet1 = book.create_worksheet :name => "根据页面显示" 
-      sheet2 = book.create_worksheet :name => "根据活动显示"
-        
-      blue = Spreadsheet::Format.new :color => :blue, :weight => :bold, :size => 10  
-      sheet1.row(0).default_format = blue  
-      
-      sheet1[0, 0] = '名称'
-      sheet1[0, 1] = '点击量'
+		    @click_pages = Click.where('record_date' => time_range).group("page")
+		    @pages = Click.where('record_date' => time_range)
+		    @click_cams = Click.where('record_date' => time_range).group("campaign")
 
-      sheet2[0, 0] = '名称'
-      sheet2[0, 1] = '点击量'
+		    
+		    count_row = 0
+		    count_col = 0
+		    xls_report = StringIO.new  
+		    book = Spreadsheet::Workbook.new
 
-      sheet1[1, 1] = count_all
-      count_row = count_row + 2
-      
-      objs.each do |obj|
-        [0, 1].each do |col|
-          if col == 0
-            sheet1[count_row,col]=Campaign.find(obj.campaign).name
-          else
-            sheet1[count_row,col]=obj.sum_campaign
-          end
-        end
-        count_row = count_row + 1
-      end  
-      
-      filepath=Rails.root+"download/triffs/#{(yd).strftime('%Y-%m-%d')}-#{model}.xls" 
-      book.write filepath
-      xls_report.string
-    end
+		    sheet1 = book.create_worksheet :name => "根据页面显示" 
+		    sheet2 = book.create_worksheet :name => "根据活动显示"
+		        
+		    blue = Spreadsheet::Format.new :color => :blue, :weight => :bold
+		    sheet1.row(0).default_format = blue
+		    sheet2.row(0).default_format = blue  
+		      
+		    sheet1[0, 0] = '页面'
+		    sheet1[0, 1] = '位置'
+		    sheet1[0, 2] = '分类'
+		    sheet1[0, 3] = '父分类'
+		    sheet1[0, 4] = '活动'
+		    sheet1[0, 5] = '点击量'
+
+		    sheet1[1, 5] = count_all
+		    count_row = count_row + 2
+		    
+			@click_pages.each do |obj|
+				@pages.where("page = #{obj.page}").order("clicks DESC").each do |page|
+					sheet1[count_row, 0] = WebPage.find(page.page).content
+				    sheet1[count_row, 1] = Position.find(page.position).name
+				    sheet1[count_row, 2] = TagCategory.find(page.up_category).name
+				    sheet1[count_row, 3] = TagCategory.find(page.category).name
+				    sheet1[count_row, 4] = Campaign.find(page.campaign).name
+				    sheet1[count_row, 5] = page.clicks
+				    count_row = count_row + 1
+				end
+			end  
+
+
+			
+			sheet2[0, 0] = '活动'
+			sheet2[0, 1] = '页面'
+		    sheet2[0, 2] = '位置'
+		    sheet2[0, 3] = '分类'
+		    sheet2[0, 4] = '父分类'
+		    sheet2[0, 5] = '点击量'
+
+		    sheet2[1, 5] = count_all
+		    count_row = 2
+
+			@click_cams.each do |obj|
+				@pages.where("campaign = #{obj.campaign}").order("clicks DESC").each do |page|
+					sheet2[count_row, 0] = Campaign.find(page.campaign).name
+					sheet2[count_row, 1] = WebPage.find(page.page).content
+				    sheet2[count_row, 2] = Position.find(page.position).name
+				    sheet2[count_row, 3] = TagCategory.find(page.up_category).name
+				    sheet2[count_row, 4] = TagCategory.find(page.category).name
+				    sheet2[count_row, 5] = page.clicks
+				    count_row = count_row + 1
+				end
+			end
+
+			filepath=Rails.root+"download/triffs/#{(yd).strftime('%Y-%m-%d')}-#{model}.xls" 
+			book.write filepath
+			xls_report.string
+		end
 
 end
